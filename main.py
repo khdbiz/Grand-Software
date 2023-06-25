7358efa1f15dda010315d939d189a9a7d7750870
khdbiz <136955359+khdbiz@users.noreply.github.com>
2023-06-25 17:15:48 +0000

    Dropdown menu made

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
-  return "Hello, Khd"
+  return render_template('home.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
+  
+  str(vehicle)
